f9b95038c84c5b6d2899df82f58f5b772b536ead
shunkat <shunkat@icloud.com>
2024-04-04 10:16:11 +0900

    flutter側を実装

--- lib/main.dart
+++ lib/main.dart
@@ -32,14 +32,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static const platform = MethodChannel('samples.flutter.dev/battery');
   // Get battery level.
   String _batteryLevel = 'Unknown battery level.';
 
   Future<void> _getBatteryLevel() async {
+    final api = BatteryHostApi();
     String batteryLevel;
     try {
-      final result = await platform.invokeMethod<int>('getBatteryLevel');
+      final result = await api.getBatteryLevel();
       batteryLevel = 'Battery level at $result % .';
     } on PlatformException catch (e) {
       batteryLevel = "Failed to get battery level: '${e.message}'.";
